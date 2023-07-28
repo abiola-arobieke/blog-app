3c13d1da24585b22c14c7d98cbe54f137270dc03
abiola <abiola.arobieke@gmail.com>
2023-07-28 06:35:59 -0400

    Modify post.rb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  def update_posts_counter
-    author.update(posts_counter: author.posts.count)
-  end
+  after_save :update_post_counter
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
   end
 end
