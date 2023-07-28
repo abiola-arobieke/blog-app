bc2f01630c90ceb894e8a407d713d03273175b51
abiola <abiola.arobieke@gmail.com>
2023-07-28 05:28:49 -0400

    Fix linters error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
