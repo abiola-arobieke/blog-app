aa04a6ecfbacbef0c89d3df27e71ce9b6cc5c79e
abiola <abiola.arobieke@gmail.com>
2023-07-28 05:25:47 -0400

    Add migration files

--- db/migrate/20230726205744_add_users_ref_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersRefToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :comments, :author_id, null: false, foreign_key: true
-  end
-end
--- db/migrate/20230726211051_add_users_ref_to_likes.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersRefToLikes < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :likes, :author_id, null: false, foreign_key: true
-  end
-end
