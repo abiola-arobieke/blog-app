a50bba4e687703776a6bf268d101d812c8680e9a
abiola <abiola.arobieke@gmail.com>
2023-07-28 05:24:10 -0400

    Add post model to the project

---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ApplicationRecord
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+end
