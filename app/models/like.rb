ccc336ad961ce8a5a6f1dde8a597e13693cfaa95
abiola <abiola.arobieke@gmail.com>
2023-07-28 06:35:29 -0400

    Modify like.rb file

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,9 @@ class Like < ApplicationRecord
   has_many :author, class_name: 'User', foreign_key: 'author_id'
   has_many :post
 
-  def update_likes_count
-    post.update(likes_counter: post.likes.count)
+  after_save :update_like_counter
+
+  def update_like_count
+    author.increment!(:likes_counter)
   end
 end
