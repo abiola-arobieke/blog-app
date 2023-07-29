c37c07b4cb33d1f0199d4dafc32b46d93c114ced
abiola <abiola.arobieke@gmail.com>
2023-07-29 04:59:15 -0400

    Add validation to like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
+  validates :author_id, uniqueness: true
+
   after_save :update_like_counter
 
   private
