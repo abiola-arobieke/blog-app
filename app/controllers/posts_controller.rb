73730293fbf1f81139ec11f439665dd8c0794a95
abiola <abiola.arobieke@gmail.com>
2023-08-05 17:18:27 -0400

    Modify posts_contoller.rb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,28 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_path(@post.author_id), notice: 'Post has been created.'
+    else
+      flash.now[:alert] = 'Error! Unable to create a post.'
+      render :show
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
