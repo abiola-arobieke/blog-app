3e2ae7300f4eb47a5e31736a156ee394db3d4a92
abiola <abiola.arobieke@gmail.com>
2023-08-10 19:30:53 -0400

    Modify application_controller.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    before_action :authenticate_user!
-    before_action :update_allowed_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def update_allowed_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :bio, :photo, :password) }
-        devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:name, :email, :password, :current_password)
-        end
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :bio, :photo, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password)
     end
+  end
 end
