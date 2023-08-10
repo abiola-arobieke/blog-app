fae98c24c4c2dd8e915ce49f2f14a14381400529
Sameed Mohsin <sameedmohsin22@hotmail.com>
2023-08-11 02:59:10 +0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :update_allowed_parameters, if: :devise_controller?
 
-    
+    protected
+
+    def update_allowed_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :bio, :photo, :password) }
+        devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :email, :password, :current_password)
+        end
+    end
 end
