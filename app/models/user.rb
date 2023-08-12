78f66dd3fd0641abe5af30705fbf08dd502a871b
abiola <abiola.arobieke@gmail.com>
2023-08-12 16:18:18 -0400

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  after_create :generate_api_token
+
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
@@ -14,5 +17,10 @@ class User < ApplicationRecord
 
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def generate_api_token
+    self.api_token = Devise.friendly_token
+    save
   end
 end
