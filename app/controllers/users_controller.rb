6873348e3a2183f9f0d31c72681228679d3cf0d4
abiola <abiola.arobieke@gmail.com>
2023-08-12 16:17:30 -0400

    Modify controllers/users_controller.rb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def api_token
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: @user.api_token, status: :ok }
+      format.html {}
+    end
+  end
+
   def index
     @users = User.all
   end
